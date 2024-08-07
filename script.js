d1a1028223c52d735053fe2e1a9c75de881a2147
Ishan <ishanjaiiswal.com>
2024-08-07 05:13:17 -1200

    Final Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let authors = document.querySelector("#author");
+let images = document.querySelector("#image");
+let titles = document.querySelector("#title");
+let generate = document.querySelector("#generate");
+
+const updateDetails = (url, title, author) => {
+  images.setAttribute("src", url);
+  authors.innerHTML = author;
+  titles.innerHTML = title;
+};
+
+const generatememe = () => {
+  fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then((data) => {
+      updateDetails(data.url, data.title, data.author);
+    });
+};
+generate.addEventListener("click", generatememe);
